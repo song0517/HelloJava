d4ece4d538c6768b5a783a3e496cc447610e03cf
song <dsd1897@naver.com>
2022-09-29 09:00:58 +0900

    과제추가

--- P20220928/src/co/edu/variable/SortExe.java
+++ P20220928/src/co/edu/variable/SortExe.java
@@ -9,14 +9,47 @@ public class SortExe {
 		// 케이스 : n1 > n2 > n3  => 풀기  break위치 바꾸는 연습?
 		// 케이스 : n1 < n2 < n3  => 풀기
 		
-		if(numAry[0] > numAry[1]) {   //34와 21을 비교  32>21
-			int temp = numAry[0]; //34
-			numAry[0] = numAry[1]; //21
-			numAry[1] = temp; //34
-		}
-		
-		for(int n : numAry) {
-			System.out.println(n);
-		}
+//		if(numAry[0] > numAry[1]) {   //34와 21을 비교  32>21
+//			int temp = numAry[0]; //34
+//			numAry[0] = numAry[1]; //21
+//			numAry[1] = temp; //34
+//		}
+//		
+//		for(int n : numAry) {
+//			System.out.println(n);
+//		}
+//		
+
+        int temp=0;
+
+        for (int i = 0; i < numAry.length; i++) {
+            for (int j = i+1; j < numAry.length; j++) {
+                if(numAry[i] > numAry[j]) {
+                    temp = numAry[i];
+                    numAry[i]=numAry[j];
+                    numAry[j]=temp;
+                }
+            }
+        }
+        for(int i : numAry) {
+            System.out.println(i);
+        }
+        System.out.println("------------------");
+
+        temp=0;
+
+        for (int i = 0; i < numAry.length; i++) {
+            for (int j = i+1; j < numAry.length; j++) {
+                if(numAry[i] < numAry[j]) {
+                    temp = numAry[i];
+                    numAry[i]=numAry[j];
+                    numAry[j]=temp;
+                }
+            }
+        }
+
+        for(int i : numAry) {
+            System.out.println(i);
+        }
 	}
 }
