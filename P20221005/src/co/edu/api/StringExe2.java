e10a610574fd6c5593270c733aaaee0159e6210f
song <dsd1897@naver.com>
2022-10-06 17:50:00 +0900

    20221006(API)

--- P20221005/src/co/edu/api/StringExe2.java
+++ P20221005/src/co/edu/api/StringExe2.java
@@ -7,24 +7,30 @@ public class StringExe2 {
 //		System.out.println("abcDEF".toUpperCase());
 		while (true) {
 			try {
-				int bytes = System.in.read();  //abd -> 아스키코드로 읽어옴.. 65(A)~90(Z), 97(a)~122(z)
+				int bytes = System.in.read(); // abd -> 아스키코드로 읽어옴.. 65(A)~90(Z), 97(a)~122(z)
 				
-				if(bytes == 122){
+				if (bytes == 122) {
 					break;
 				}
-				
+					changeCapital(bytes);
+
 				System.out.println("입력값: " + bytes);
-				} catch (IOException e) {
-					e.printStackTrace();
+			} catch (IOException e) {
+				e.printStackTrace();
 			}
 		}
 	}
-	
-	public static void changeCapital(String msg) {
+
+	public static void changeCapital(int bytes) {
 		// 대문자는 소문자로, 소문자는 대문자로
 		// aBcD => AbCd 97:65 (차이는 32)
+		if(bytes >= 97) {
+			bytes -= 32;
+		} else if(bytes <= 90){
+			bytes += 32;
+		}
 		
 		
-		
-	}
+	
+}
 }
